6fc1cfd0643a9cc217f08d502b519c394233f9b7
einsteinisaac2 <einsteinisaac2@outlook.com>
2022-02-21 11:51:34 +0800

    修改配置

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-host="159.138.123.135"
+host="127.0.0.1"
 port=3306
 user="btca"
 password="1234qwer"
 db="btca"
 url="http://127.0.0.1:9904"
-forkid="00000001cf57212c59faebd3409ae04291c7628cb0d7de8897682a9ac587627e"
+forkid="000000011afb1fd51e4df1254f32c269b32bffee04def334079ef1c05053cf5e"
